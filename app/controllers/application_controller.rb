4bf006b9e5abbc5dfae116239afc8871dfd4e371
Thomás Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-03-17 00:01:13 -0300

    configuring default parameters of devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::API
-  include DeviseTokenAuth::Concerns::SetUserByToken
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :password_confirmation])
+  end
 end
