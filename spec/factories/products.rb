92de701cedcb21d85363871749e46cfe73d31cb0
Thomás Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-04-23 23:16:17 -0300

    fix validation of productable in spec file

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,9 @@ FactoryBot.define do
     sequence(:name) { |index| "Product #{index}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 10.0..400.0) }
-    productable { nil }
+
+    after :build do |product|
+      product.productable = create(:game)
+    end
   end
 end
