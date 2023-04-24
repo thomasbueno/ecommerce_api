4c1fb8aeb1ba8b21f2bcf38b9d9b10d728ff76e7
Thomás Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-04-23 23:20:07 -0300

    add has_many relation in system_requirement

--- spec/models/system_requirement_spec.rb
+++ spec/models/system_requirement_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe SystemRequirement, type: :model do
+  it { is_expected.to have_many(:games).dependent(:restrict_with_error) }
+
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:operational_system) }
   it { is_expected.to validate_presence_of(:storage) }
