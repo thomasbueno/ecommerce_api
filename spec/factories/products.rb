cf50faaf6133c85aef1a693bfe0aba7fef558b57
Thomás Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-04-25 01:07:01 -0300

    add image association in Product model

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     sequence(:name) { |index| "Product #{index}" }
     description { Faker::Lorem.paragraph }
     price { Faker::Commerce.price(range: 10.0..400.0) }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/images/product_image.png')) }
 
     after :build do |product|
       product.productable = create(:game)
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -10,6 +10,7 @@ RSpec.describe Product, type: :model do
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_presence_of(:description) }
   it { is_expected.to validate_presence_of(:price) }
+  it { is_expected.to validate_presence_of(:image) }
 
   it { is_expected.to validate_uniqueness_of(:name).case_insensitive }
   it { is_expected.to validate_numericality_of(:price).is_greater_than(0) }
