cd407154e4b5f258a0ac4efa91733e2e52d66002
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-07-26 23:19:27 -0300

    add load_category method to improving syntax

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,6 +1,8 @@
 module Admin
   module V1
     class CategoriesController < ApiController
+      before_action :load_category, only: [:update, :destroy]
+
       def index
         @categories = Category.all
       end
@@ -13,21 +15,21 @@ module Admin
       end
 
       def update
-        @category = Category.find(params[:id])
         @category.attributes = category_params
-
         save_category!
       end
 
       def destroy
-        @category = Category.find(params[:id])
         @category.destroy!
-      
       rescue
         render_error(fields: @category.errors.messages)
       end
 
       private
+
+      def load_category
+        @category = Category.find(params[:id])
+      end
 
       def category_params
         return {} unless params.has_key?(:category)
