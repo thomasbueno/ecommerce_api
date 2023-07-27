38848f5136a1ca49e56643b084c841148e6c6cb0
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-07-26 23:07:20 -0300

    creating update method to categories

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -12,6 +12,13 @@ module Admin
         save_category!
       end
 
+      def update
+        @category = Category.find(params[:id])
+        @category.attributes = category_params
+        
+        save_category!
+      end
+
       private
 
       def category_params
