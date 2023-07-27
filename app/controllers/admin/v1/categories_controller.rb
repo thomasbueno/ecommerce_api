c93f72b0911864db9e3f6255fc532cabe551aa8c
Thomás Escolástico Bueno <62290004+thomasbueno@users.noreply.github.com>
2023-07-26 22:52:32 -0300

    creating private function to save category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -9,12 +9,7 @@ module Admin
         @category = Category.new
         @category.attributes = category_params
 
-        @category.save!
-
-        render :show
-
-      rescue
-        render_error(fields: @category.errors.messages)
+        save_category!
       end
 
       private
@@ -23,6 +18,13 @@ module Admin
         return {} unless params.has_key?(:category)
 
         params.require(:category).permit(:name)
+      end
+
+      def save_category!
+        @category.save!
+        render :show
+      rescue
+        render_error(fields: @category.errors.messages)
       end
     end
   end
